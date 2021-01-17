cf431ee6f47c7ac8acc03ded16e75ccfdc0724c4
Johan Sjöland <johan@sjoland.com>
2021-01-17 23:28:01 +0100

    Refactor

--- src/Image.php
+++ src/Image.php
@@ -2,10 +2,269 @@
 
 namespace ABetter\Image;
 
+use Imagick;
 //use Illuminate\Database\Eloquent\Model AS BaseModel;
 
 class Image {
 
+	public static $default = [
+		'storage' => 'cache/image',
+		'return' => 'public',
+		'request' => NULL,
+		'protocol' => NULL,
+		'domain' => NULL,
+		'path' => NULL,
+		'filename' => NULL,
+		'type' => NULL,
+		'remote' => NULL,
+		'style' => NULL,
+		'source' => NULL,
+		'target' => NULL,
+		'process' => NULL,
+		'styles' => NULL,
+	];
 
+	// ---
+
+	public static function get() {
+		$opt = self::options(func_get_args());
+		$opt['source'] = self::source($opt);
+		$opt['target'] = self::target($opt);
+		if (is_file($opt['source']) && !is_file($opt['target'])) {
+			if (empty($opt['style']) || $opt['style'] == 'x') {
+				$opt['target'] = $opt['source'];
+			} else {
+				$opt['styles'] = self::styles($opt['style']);
+				$opt['process'] = self::imagick(
+					$opt['source'],
+					$opt['target'],
+					$opt['type'],
+					$opt['styles']
+				);
+			}
+		}
+		$opt['is_source'] = (is_file($opt['source']));
+		$opt['is_target'] = (is_file($opt['target']));
+		if ($opt['return'] == 'public') return self::public($opt);
+		return (is_file($opt['target'])) ? $opt['target'] : NULL;
+	}
+
+	// ---
+
+	public static function source($opt=[]) {
+		$source = (preg_match('/^\/cache\//',$opt['path'])) ? storage_path() : public_path();
+		$source .= $opt['path'].$opt['filename'].'.'.$opt['type'];
+		if ($opt['remote'] && !is_file($source)) {
+			$source = $opt['storage'].'/'.$opt['cachekey'].'.'.$opt['type'];
+			if ($content = @file_get_contents($opt['remote'])) {
+				@file_put_contents($source,$content);
+			}
+		}
+		return $source;
+	}
+
+	public static function target($opt=[]) {
+		return $opt['storage'].'/'.$opt['cachekey'].'.'.$opt['style'].'.'.$opt['type'];
+	}
+
+	public static function public($opt=[]) {
+		return $opt['path'].$opt['filename'].'.'.$opt['type'];
+	}
+
+	// ---
+
+	public static function cachekey($opt=[]) {
+		$cachekey = trim(str_replace(['/'],['.'],$opt['domain'].$opt['path'].$opt['filename']),'.');
+		return preg_replace('/\.[^.\s]{3,4}$/','',$cachekey);
+	}
+
+	// ---
+
+	public static function filetype($url,$reverse=FALSE) {
+		return self::format(($headers = @get_headers($url,1)) ? $headers['Content-Type']??"" : "image/jpeg",TRUE);
+	}
+
+	public static function format($ext,$reverse=FALSE) {
+		$formats = [
+			'jpeg' => 'image/jpeg',
+			'jpg' => 'image/jpeg',
+			'png' => 'image/png',
+			'gif' => 'image/gif',
+		];
+		$formats = ($reverse) ? array_flip($formats) : $formats;
+		return $formats[$ext] ?? reset($formats);
+	}
+
+	// ---
+
+	public static function options($args=[]) {
+		$opt = self::$default;
+		foreach ($args AS $arg) if (is_array($arg)) $opt = array_merge($opt,$arg);
+		if (is_string($args[0]??NULL)) {
+			$opt['request'] = $args[0];
+		}
+		if (is_string($args[1]??NULL)) {
+			$opt['style'] = $args[1];
+		}
+		if (is_string($args[2]??NULL)) {
+			$opt['return'] = $args[2];
+		}
+		$opt['path'] = rtrim(pathinfo($opt['request'],PATHINFO_DIRNAME),'/').'/';
+		$opt['filename'] = pathinfo($opt['request'],PATHINFO_FILENAME);
+		$opt['type'] = pathinfo($opt['request'],PATHINFO_EXTENSION);
+		if (preg_match('/https?\:\/\//',$opt['request'])) {
+			$opt['domain'] = parse_url($opt['request'],PHP_URL_HOST);
+			$opt['protocol'] = parse_url($opt['request'],PHP_URL_SCHEME).'://';
+			$opt['path'] = rtrim(parse_url($opt['path'],PHP_URL_PATH),'/').'/';
+			$opt['remote'] = ($opt['domain'] !== parse_url(env('APP_URL'),PHP_URL_HOST)) ? $opt['protocol'].$opt['domain'].$opt['path'] : FALSE;
+			if (!$opt['type']) $opt['type'] = self::format($opt['remote'],TRUE);
+			if (!$opt['remote']) $opt['domain'] = "";
+		}
+		$opt['cachekey'] = self::cachekey($opt);
+		$opt['storage'] = storage_path($opt['storage']);
+		if (!is_dir($opt['storage'])) \File::makeDirectory($opt['storage'],0777,TRUE);
+		return $opt;
+	}
+
+	// ----
+
+	public static function imagick($source,$target,$type,$style) {
+		$imagick = new Imagick($source);
+		self::imagickResize($imagick,$style);
+		self::imagickFilter($imagick,$style);
+		if ($type == 'png' || $type == 'gif') {
+			$imagick->setImageCompression(Imagick::COMPRESSION_UNDEFINED);
+			$imagick->setImageCompressionQuality(0);
+		} else if ($type == 'jpg' || $type == 'jpeg') {
+			$imagick->setImageCompression(Imagick::COMPRESSION_JPEG);
+			$imagick->setImageCompressionQuality(70);
+			$imagick->setImageBackgroundColor($style['background']);
+			$imagick->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE);
+			$imagick = $imagick->mergeImageLayers(Imagick::LAYERMETHOD_FLATTEN);
+		}
+		$imagick->stripImage();
+		$imagick->writeImage($target);
+		$imagick->clear();
+		return TRUE;
+	}
+
+	public static function imagickResize($imagick,$style) {
+		$source_w = $imagick->getImageWidth();
+		$source_h = $imagick->getImageHeight();
+		$target_w = floor($style['w']);
+		$target_h = floor($style['h']);
+		// ---
+		if (empty($target_w) && empty($target_h)) {
+			$target_w = $source_w;
+			$target_h = $source_h;
+		}
+		// ---
+		if ($target_w > $target_h) {
+			$resize_w = $target_w;
+			$resize_h = floor($source_h * ($target_w / $source_w));
+		} else {
+			$resize_w = floor($source_w * ($target_h / $source_h));
+			$resize_h = $target_h;
+		}
+		$imagick->resizeImage($resize_w, $resize_h, Imagick::FILTER_LANCZOS, 0.9);
+		// ---
+		if ($target_w == 0 || $target_h == 0) return $imagick;
+		// ---
+		$crop_w = $target_w;
+		$crop_h = $target_h;
+		$crop_x = 0;
+		$crop_y = 0;
+		$crop_align = $style['align'];
+		// ---
+		if ($resize_w > $crop_w) {
+			$crop_x = floor(($resize_w - $crop_w) / 2);
+			if (preg_match('/left/',$crop_align)) $crop_x = 0;
+			if (preg_match('/right/',$crop_align)) $crop_x = floor($resize_w - $crop_w);
+			$imagick->cropImage($crop_w, $target_h, $crop_x, 0);
+		}
+		if ($resize_h > $crop_h) {
+			$crop_y = floor(($resize_h - $crop_h) / 2);
+			if (preg_match('/top/',$crop_align)) $crop_y = 0;
+			if (preg_match('/bottom/',$crop_align)) $crop_y = floor($resize_h - $crop_h);
+			$imagick->cropImage($target_w, $crop_h, 0, $crop_y);
+		}
+		$imagick->setImagePage(0, 0, 0, 0); // Reset canvas
+		// ---
+		return $imagick;
+	}
+
+	public static function imagickFilter($imagick,$style) {
+		if ($style['filter'] == 'grayscale') {
+			$imagick->transformimagecolorspace(Imagick::COLORSPACE_GRAY);
+		}
+		if ($style['filter'] == 'blur') {
+			$val = ($style['value']) ? (int) $style['value'] : 10;
+			$imagick->blurImage($val,$val);
+		}
+		if ($style['filter'] == 'lighter') {
+			$val = ($style['value']) ? (int) $style['value'] : 25;
+			$imagick->brightnessContrastImage($val, 0);
+		}
+		if ($style['filter'] == 'darker') {
+			$val = ($style['value']) ? (int) $style['value'] : 25;
+			$imagick->brightnessContrastImage(0 - $val, 0);
+		}
+		return $imagick;
+	}
+
+	public static function styles($string,$style=[]) {
+		$style['args'] = explode('-',$string);
+		$style['dimension'] = (isset($style['args'][0])) ? $style['args'][0] : NULL;
+		$style['align'] = (isset($style['args'][1])) ? $style['args'][1] : NULL;
+		$style['filter'] = (isset($style['args'][2])) ? $style['args'][2] : NULL;
+		$style['value'] = (isset($style['args'][3])) ? $style['args'][3] : NULL;
+		$style['background'] = NULL; //'#000';
+		// ---
+		$style['type'] = '';
+		$style['w'] = 0;
+		$style['h'] = 0;
+		if (preg_match('/x/',$style['dimension'])) {
+			$style['type'] = 'x';
+			list($style['w'],$style['h']) = explode('x',$style['dimension']);
+			$style['w'] = (int) $style['w'];
+			$style['h'] = (int) $style['h'];
+		} elseif (preg_match('/^w/',$style['dimension'])) {
+			$style['type'] = 'w';
+			$style['w'] = (int) ltrim($style['dimension'],'w');
+		} elseif (preg_match('/^h/',$style['dimension'])) {
+			$style['type'] = 'h';
+			$style['h'] = (int) ltrim($style['dimension'],'h');
+		} elseif (is_numeric($style['dimension'])) {
+			$style['type'] = 's';
+			$style['w'] = (int) $style['dimension'];
+			$style['h'] = (int) $style['dimension'];
+		}
+		// ---
+		$aligns = array(
+			'c' => 'center', 'm' => 'center', 'middle' => 'center', 'center' => 'center',
+			't' => 'top', 'top' => 'top',
+			'b' => 'bottom', 'bottom' => 'bottom',
+			'l' => 'left', 'left' => 'left',
+			'r' => 'right', 'right' => 'right',
+			'lt' => 'lefttop', 'tl' => 'lefttop', 'topleft' => 'lefttop', 'lefttop' => 'lefttop',
+			'lb' => 'leftbottom', 'bl' => 'leftbottom', 'bottomleft' => 'leftbottom', 'leftbottom' => 'leftbottom',
+			'rt' => 'righttop', 'tr' => 'righttop', 'topright' => 'righttop', 'righttop' => 'righttop',
+			'rb' => 'rightbottom', 'br' => 'rightbottom', 'bottomright' => 'rightbottom', 'rightbottom' => 'rightbottom'
+		);
+		$style['align'] = (isset($aligns[$style['align']])) ? $aligns[$style['align']] : 'center';
+		if ($style['align'] == 'box' && $style['filter']) $style['background'] = $style['filter'];
+		// ---
+		$filters = array(
+			'g' => 'grayscale', 'gray' => 'grayscale', 'grayscale' => 'grayscale',
+			'b' => 'blur', 'blur' => 'blur',
+			'l' => 'lighter', 'light' => 'lighter', 'lighter' => 'lighter',
+			'd' => 'darker', 'dark' => 'darker', 'darker' => 'darker'
+		);
+		$style['filter'] = (isset($filters[$style['filter']])) ? $filters[$style['filter']] : NULL;
+		// ---
+		$style['background'] = $style['background'] ?? '#FFFFFF';
+		// ---
+		return $style;
+	}
 
 }
--- src/ImageController.php
+++ src/ImageController.php
@@ -2,11 +2,12 @@
 
 namespace ABetter\Image;
 
+use ABetter\Image\Image;
 use Illuminate\Routing\Controller as BaseController;
 
 class ImageController extends BaseController {
 
-	protected $image;
+	public $image;
 
 	public function handle($style,$path) {
 
@@ -14,66 +15,25 @@ class ImageController extends BaseController {
 			'expire' => '1 year',
 			'path' => '/'.trim($path,'/'),
 			'style' => $style,
+			'location' => NULL,
+			'public' => NULL,
 		];
 
-		/*
+		$this->image['location'] = Image::get(
+			$this->image['path'],
+			$this->image['style'],
+			'location'
+		);
 
-		// Parse
-		$this->image['ext'] = pathinfo($this->image['path'],PATHINFO_EXTENSION);
-		$this->image['name'] = pathinfo($this->image['path'],PATHINFO_FILENAME);
-		if (($this->image['conv'] = strtolower(pathinfo($this->image['name'],PATHINFO_EXTENSION))) && in_array($this->image['conv'],['jpg','jpeg','png'])) {
-			$this->image['ext'] = $this->image['conv'];
-			$this->image['name'] = pathinfo($this->image['name'],PATHINFO_FILENAME);
+		if (!is_file($this->image['location'])) {
+			return abort(404);
 		}
 
-		// Location
-		$this->image['source'] = $this->imageFileSearch($this->image);
-		$this->image['target'] = preg_replace('/\.([^\.]+)$/',".{$this->image['style']}.$1",storage_path('cache').$this->image['path']);
+		$GLOBALS['HEADERS']['expire'] = $this->image['expire'];
+		$GLOBALS['HEADERS']['modified'] = @filemtime($this->image['location']);
 
-		// Process
-		if (!is_file($this->image['target'])) {
-			if (!is_file($this->image['source'])) {
-				return abort(404);
-			} else {
-				$this->image['processed'] = $this->imageProcess($this->image);
-			}
-		}
+		return response()->file($this->image['location']);
 
-		// Response
-		$this->image['modified'] = @filemtime($this->image['target']);
-		$this->image['expire'] = (!is_numeric($this->image['expire'])) ? strtotime($this->image['expire'],0) : $this->image['expire'];
-		$this->image['expires'] = @gmdate('D, d M Y H:i:s \G\M\T', $this->image['modified'] + $this->image['expire']);
-		$this->image['headers'] = [
-			"cache-control" => "public, max-age={$this->image['expire']}",
-			"expires" => "{$this->image['expires']}",
-		];
-
-		return response()->file($this->image['target'], $this->image['headers']);
-		*/
-
-    }
-
-	// ---
-
-	/*
-
-	public function imageFileSearch($image,$found=NULL) {
-		$dir = dirname(public_path().$image['path']);
-		if (!is_dir($dir)) return FALSE;
-		foreach (scandir($dir) AS $f) {
-			if ($found || in_array($f,['.','..'])) continue;
-			if ($f == "{$image['name']}.{$image['ext']}") $found = $f;
-			if (pathinfo($f,PATHINFO_FILENAME) == $image['name']) $found = $f;
-		}
-		return ($found) ? realpath($dir.DIRECTORY_SEPARATOR.$found) : FALSE;
 	}
-
-	// ---
-
-	public function imageProcess($image) {
-		return \_imageMagick($image['source'],$image['target'],$image['style']);
-	}
-
-	*/
 
 }
